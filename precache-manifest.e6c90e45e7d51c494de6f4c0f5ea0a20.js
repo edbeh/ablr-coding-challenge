51d583b5d23d9de23a5491d2ed5bb4246b0e22e2
edbeh <edbeh89@gmail.com>
2020-07-01 16:36:10 +0800

    Updates

---
+++ precache-manifest.e6c90e45e7d51c494de6f4c0f5ea0a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61846763a81e12636e3cda4a97379606",
+    "url": "/ablr-coding-challenge/index.html"
+  },
+  {
+    "revision": "899434a244df208a3675",
+    "url": "/ablr-coding-challenge/static/css/main.5dc9ec70.chunk.css"
+  },
+  {
+    "revision": "1a0672c5da73b86f2222",
+    "url": "/ablr-coding-challenge/static/js/2.1068b787.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/ablr-coding-challenge/static/js/2.1068b787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "899434a244df208a3675",
+    "url": "/ablr-coding-challenge/static/js/main.a741da77.chunk.js"
+  },
+  {
+    "revision": "48c8bca5d8bc7b1c5a85",
+    "url": "/ablr-coding-challenge/static/js/runtime-main.d5bd92b4.js"
+  }
+]);
